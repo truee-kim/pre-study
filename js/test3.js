b2b85c78795ccbc3aa2621a2181f24ffea79b848
truee-kim <kjs3597@naver.com>
2022-01-01 23:33:53 +0900

    주석처리

--- js/test3.js
+++ js/test3.js
@@ -8,13 +8,13 @@ let text = document.getElementById("text");
 //ip에 입력한 값을 text에 추가할 수 있게
 ip.addEventListener("keydown", function(){
     text.innerText = ip.value;
-})
+});
 
 let person = {
     name: "jisung park",
     age: 21,
     team: "nct dream"
-};
+}
 
 let info = document.getElementById("info");
 let p = "";
@@ -22,5 +22,15 @@ for(let i in person){
     // console.log("숫자" + i);
      p += person[i] + "<br>";
 }
-console.log(p);
+// console.log(p);
 info.innerHTML = p;
+
+
+/*
+function foo (a) {
+    return a + 3;
+  }
+  
+  const arr = [ foo(1), foo(2), foo(3) ];
+  
+  console.log(arr); // [4,5,6]??? 땡 ...  Array(3)*/
